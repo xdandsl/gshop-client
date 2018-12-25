ca3d34e6f6513f69ba5b5915224314f8ee127db6
xdandsl <15733221796@163.com>
2018-12-25 22:18:55 +0800

    shopgoods的分类和食物列表的滚动条完成。利用better-scroll完成

--- src/store/modules/shop.js
+++ src/store/modules/shop.js
@@ -10,19 +10,21 @@ import {
 import { reqGoods , reqRatings, reqInfo} from '../../api/index'
 
 const state = {
-  goods: [],
-  ratings: [],
-  info: {}
+  goods: [], //食物分类
+  ratings: [], //商家评价
+  info: {} //商家信息
 }
 
 const actions = {
   //获取食品goods
-  async getGoods({commit}){
+  async getGoods({commit},cb){
     const result = await reqGoods()
     const goods = result.data
     if(result.code === 0){
       commit(RECEIVE_GOODS , {goods})
     }
+    //等状态更新完去执行传入的回调函数(即$nextTick()，这个里面的函数等dom更新完就去创建BScroll对象)
+   typeof cb === 'function' && cb()
   },
   //获取ratings
   async getRatings({commit}){
